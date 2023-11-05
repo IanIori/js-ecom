e11cbef4dd93670574aa4b0cd3dddf3cc65bec63
Ian Iori <ianmessia@gmail.com>
2023-11-05 19:03:09 -0300

    development: business models creation

---
+++ src/models/BaseModel.js
@@ -0,0 +1,33 @@
+export default class BaseModel{
+    constructor(db, entity) {
+        this.db = db[entity]
+    }
+
+    //Database entries here 
+
+    findAll() {
+        return this.db
+    }
+
+    findById(id) {
+        return this.db.find((item) => item.id === id)
+    }
+
+    create(item) {
+        this.db.push(item)
+    }
+
+    delete(id) {
+        const index = this.db.findIndex(item => item.id == id)
+        if(index === -1) throw Error("Item not found")
+        this.db.splice(index,1)
+        return true
+    }
+
+    update(id, item) {
+        const index = this.db.findIndex(item => item.id == id)
+        if(index === -1) throw Error("Item not found")
+        this.db[index] = item
+        return true
+    }
+}
---
+++ src/models/CustomerModel.js
@@ -0,0 +1,9 @@
+import BaseModel from "./BaseModel.js";
+
+export default class CustomerModel extends BaseModel{
+    constructor(db) {
+        super(db,'customers')
+    }
+    
+    //
+}
